0d7688c30c83599d6a3f4fa1cb0b4f8be7570564
Simon Damminga <99039973@mydavinci.nl>
2016-12-08 08:36:42 +0100

    layout af

---
+++ index.js
@@ -0,0 +1,22 @@
+var numbers = [
+	document.getElementById('numberOne'),
+	document.getElementById('numberTwo'),
+	document.getElementById('numberThree'),
+	document.getElementById('numberFour'), 
+	document.getElementById('numberFive'), 
+	document.getElementById('numberSix'),
+	document.getElementById('numberSeven'),
+	document.getElementById('numberEight'),
+	document.getElementById('numberNine'),
+	document.getElementById('numberZero')
+]
+var caracters = [
+	document.getElementById('plus'),
+	document.getElementById('minus'),
+	document.getElementById('times'),
+	document.getElementById('devided'),
+	document.getElementById('equals'),
+	document.getElementById('point'),
+	document.getElementById('reset')
+]
+numbers[0].onclick = console.log('doei');
