6cb5b854d6bd00f302b10152519a67f0c2d375db
Simon Damminga <99039973@mydavinci.nl>
2016-12-15 12:02:12 +0100

    update

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
+var sum = [];
+var count = 0;
+var box = document.getElementById('display-text');
+var ans = document.getElementById('answer-text');
+var num = "";
+function input(x){
+	box.value += x;
+	if(x == 'c'){
+		box.value = '';
+		ans.value = '';
+		sum = [];
+		count = 0;
+	}else{
+		num += x;
+	}
+	console.log(sum);
+}
+function operator(o){
+	console.log(sum);
 
-window.onload = function(){
-var nee = "DAAG!"
-var awnser,
-	calculation,
-	limt,
-	oparator,
-
-	calculation = document.getElementById('display');
-	var keys = document.getElementsByClassName('button');
-
-	var len = keys.lenght;
-
-	for(var i = 0; i < len; i++){
-		keys[i].addEventListener("click", function() {
-			num = this.value;
-			output = calculation.innerHTML +=num;
-			limt = output.lenght;
-			if(limt > 16){
-				alert(nee);
-			}
-		}, false)
+	sum.push(num);
+	box.value += o;
+	count++;
+	sum.push(o);
+	num = "";
+	console.log(sum);
+	if(count == 2){
+		answer();	
+		sum.splice(1, 1, o);
+		box.value = ans.value;
+		box.value += o;
 	}
 }
-
-
-
+function answer(){
+	console.log(sum);
+	sum.push(num);
+	num = "";
+	var num1 = parseFloat(sum[0]);
+	var num2 = parseFloat(sum[2]);
+	if(sum[1] == '+'){
+		ans.value = num1+num2;
+	}else if(sum[1] == '-'){
+		ans.value = num1-num2;
+	}else if(sum[1] == '*'){
+		ans.value = num1*num2;
+	}else if(sum[1] == '/'){
+		ans.value = num1/num2;
+	}
+	console.log(sum);
+	sum = [];
+	console.log(sum);
+	count = 1;
+	sum.push(ans.value);
+}
